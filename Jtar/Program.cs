25a48c8c0c1b411292227e862c9ade64e7caac82
Jan Koubek <koubek.jenda@icloud.com>
2025-11-23 16:14:02 +0100

    Implemented command line arguments

--- Jtar/Program.cs
+++ Jtar/Program.cs
@@ -1,23 +1,84 @@
-﻿using Jtar.Compression.Compressor;
+﻿using System.CommandLine;
+using Jtar.Compression;
+using Jtar.Compression.Compressor;
 using Jtar.Logging;
 
 namespace Jtar;
 
 class Program
 {
-    static void Main(string[] args)
+    static int Main(string[] args)
     {
-        Logger.ShowDebugMessages = true;
-        // TODO: Block absolute paths
-        
-        Compression.CompressionContextBuilder builder = new Compression.CompressionContextBuilder();
-        Compression.CompressionContext context = builder
-            .SetInputFiles(new string[] { "obj" })
-            .SetOutputFile("output.tar")
-            //.SetCompressor(new ZstdCompressor())
-            .SetThreadCount(11)
-            .Build();
+        RootCommand rootCommand = new("jtar");
 
-        context.Compress();
+        Option<int> threadCountOption = new("--threadCount", "-t")
+        {
+            Description = "The amount of threads used for compression"
+        };
+        rootCommand.Options.Add(threadCountOption);
+
+        Option<string> outputFileOption = new("--output", "-o")
+        {
+            Description = "The filepath where the resulting file will be saved"
+        };
+        rootCommand.Options.Add(outputFileOption);
+
+        var fileArgument = new Argument<IEnumerable<string>>("file")
+        {
+            Description = "The file to package"
+        };
+        rootCommand.Arguments.Add(fileArgument);
+
+        var debugOption = new Option<bool>("--debug", "-d")
+        {
+            Description = "Print out debug information"
+        };
+        rootCommand.Options.Add(debugOption);
+
+        var noCompressionOption = new Option<bool>("--noCompression", "-n")
+        {
+            Description = "Disables compression"
+        };
+        rootCommand.Options.Add(noCompressionOption);
+
+        ParseResult parseResult = rootCommand.Parse(args);
+        if (parseResult.Errors.Count == 0 && parseResult.GetValue(fileArgument) is IEnumerable<string> files)
+        {
+            if (files.Count() < 1)
+            {
+                Logger.Log(LogType.Error, "No files specified!");
+                return 1;
+            }
+            Logger.ShowDebugMessages = parseResult.GetValue(debugOption);
+            Compression.CompressionContextBuilder builder = new CompressionContextBuilder();
+            builder.SetInputFiles(files);
+
+            var outputName = files.First() + ".tar";
+            var isCompression = !parseResult.GetValue(noCompressionOption);
+            ICompressor compressor;
+            if (isCompression)
+            {
+                outputName += ".zstd";
+                compressor = new ZstdCompressor();
+            }
+            else
+            {
+                compressor = new NoCompressor();
+            }
+            builder.SetCompressor(compressor);
+
+            var argOutputName = parseResult.GetValue(outputFileOption);
+            if (argOutputName != null && argOutputName != string.Empty) outputName = argOutputName;
+
+            var threadCount = parseResult.GetValue(threadCountOption);
+            if (threadCount > 0) builder.SetThreadCount(threadCount);
+
+            builder.SetOutputFile(outputName);
+
+            var context = builder.Build();
+            context.Compress();
+            return 0;
+        }
+        return 1;
     }
 }
