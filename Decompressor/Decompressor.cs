08d226dcbe5e908c96e4dc819121c65625bd126f
Jan Koubek <koubek.jenda@icloud.com>
2025-11-24 17:01:26 +0100

    Implemented simple decompressor for testing

---
+++ Decompressor/Decompressor.cs
@@ -0,0 +1,21 @@
+using ZstdNet;
+
+namespace Decompressor;
+
+public class Decompressor
+{
+    private readonly string _filepath;
+
+    public Decompressor(string filepath)
+    {
+        _filepath = filepath;
+    }
+
+    public void Decompress()
+    {
+        using FileStream outputFileStream = new FileStream(_filepath.Replace(".tar.zstd", ".tar"), FileMode.Create);
+        using FileStream inputFileStream = new FileStream(_filepath, FileMode.Open);
+        using DecompressionStream decompressionStream = new DecompressionStream(inputFileStream);
+        decompressionStream.CopyTo(outputFileStream);
+    }
+}
---
+++ Decompressor/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Decompressor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: Decompressor <path to .tar.zstd file>");
+                return;
+            }
+
+            string filepath = args[0];
+            Decompressor decompressor = new Decompressor(filepath);
+            decompressor.Decompress();
+
+            Console.WriteLine($"Decompressed {filepath} successfully.");
+        }
+    }
+}
