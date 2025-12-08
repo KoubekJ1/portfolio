823fdf127c3be4078077468ebf698ce11ad8df88
Jan Koubek <koubek.jenda@icloud.com>
2025-12-08 18:14:20 +0100

    Put all managers together in the decompression context

--- jtar/Jtar/Decompression/DecompressionContext.cs
+++ jtar/Jtar/Decompression/DecompressionContext.cs
@@ -1,16 +1,49 @@
+using System.Threading.Tasks;
+using Jtar.Compressor;
+using Jtar.Decompression.ChunkDecompressor;
+using Jtar.Decompression.ChunkSeparator;
+using Jtar.Decompression.Output;
+using Jtar.Logging;
+
 namespace Jtar.Decompression;
 
 public class DecompressionContext
 {
+    private readonly byte[] _compressedData;
     private readonly string _outputDir;
+    private readonly int _threadCount;
 
-    public DecompressionContext(string outputDir)
+    public DecompressionContext(byte[] compressedData, string outputDir, int threadCount)
     {
+        _compressedData = compressedData;
         _outputDir = outputDir;
+        _threadCount = threadCount;
     }
 
-    public void Decompress()
+    public async Task Decompress()
     {
-        
+        Logger.Log(LogType.Debug, "Beginning unpacking...");
+
+        // Zstd magic string (little-endian): 0xFD2FB528
+        byte[] magicString =
+        [
+            Convert.ToByte("0x28"),
+            Convert.ToByte("0xB5"),
+            Convert.ToByte("0x2F"),
+            Convert.ToByte("0xFD"),
+        ];
+        var chunkSeparatorManager = new ChunkSeparatorManager(_compressedData, magicString);
+        var chunkDecompressorManager = new ChunkDecompressorManager(new ZstdCompressor(), chunkSeparatorManager.CompressedChunks, _threadCount);
+        var outputManager = new OutputManager(chunkDecompressorManager.DecompressedChunks, Path.GetFullPath(_outputDir));
+
+        Task[] tasks =
+        [
+            Task.Run(chunkSeparatorManager.Run),
+            Task.Run(chunkDecompressorManager.Run),
+            Task.Run(outputManager.Run),
+        ];
+
+        await Task.WhenAll();
+        Logger.Log(LogType.Debug, "Finished unpacking!");
     }
 }
