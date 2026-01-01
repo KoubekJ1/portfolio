2f4d30f770fb28efc843d87dfb05553ff7f3f6c3
Jan Koubek <koubek.jenda@icloud.com>
2026-01-01 19:36:18 +0100

    Implemented report creation

---
+++ musicplayer/musicplayer/report/ReportContent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace musicplayer.report
+{
+	public class ReportContent
+	{
+		public int ArtistCount { get; set; }
+		public int AlbumCount { get; set; }
+		public int SongCount { get; set; }
+
+		public long TotalListeningTime { get; set; }
+
+		public string MostPopularArtist = "<unknown>";
+		public int MostPopularArtistListeningTime { get; set; }
+
+		public string LeastPopularArtist = "<unknown>";
+		public int LeastPopularArtistListeningTime { get; set; }
+
+		public string MostPopularAlbum = "<unknown>";
+		public int MostPopularAlbumListeningTime { get; set; }
+
+		public string LeastPopularAlbum = "<unknown>";
+		public int LeastPopularAlbumListeningTime { get; set; }
+
+		public string MostPopularSong = "<unknown>";
+		public int MostPopularSongListeningTime { get; set; }
+
+		public string LeastPopularSong = "<unknown>";
+		public int LeastPopularSongListeningTime { get; set; }
+	}
+}
--- musicplayer/views.sql
+++ musicplayer/views.sql
@@ -11,3 +11,9 @@ SELECT TOP(1) ar_name, ar_listening_time FROM artists ORDER BY ar_listening_time
 
 CREATE VIEW most_popular_artist AS
 SELECT TOP(1) ar_name, ar_listening_time FROM artists ORDER BY ar_listening_time DESC;
+
+CREATE VIEW least_popular_album AS
+SELECT TOP(1) alb_name, SUM(so_listening_time) AS 'album_listening_time' FROM albums INNER JOIN album_songs ON alb_id = as_alb_id INNER JOIN songs ON so_id = as_so_id GROUP BY alb_name ORDER BY SUM(so_listening_time) ASC;
+
+CREATE VIEW most_popular_album AS
+SELECT TOP(1) alb_name, ar_name, SUM(so_listening_time) AS 'album_listening_time' FROM albums INNER JOIN album_songs ON alb_id = as_alb_id INNER JOIN songs ON so_id = as_so_id GROUP BY alb_name ORDER BY SUM(so_listening_time) DESC;
